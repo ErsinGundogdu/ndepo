34d51c674f74c8d469f586b98ccda0c1b13bf3c0
ErsinGundogdu <ersin.gundogdu93@gmail.com>
2016-12-17 00:44:29 +0300

    ilkproje

---
+++ ilkproje/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	cout<<"hello world";
+	return 0;
+}
